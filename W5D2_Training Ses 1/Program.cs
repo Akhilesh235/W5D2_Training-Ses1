897e92013e0133f5a3e2e42f98480fd2f4387a7f
Akhilesh235 <90238925+Akhilesh235@users.noreply.github.com>
2021-10-05 11:56:45 +0800

    Background and ForegroundThread

--- W5D2_Training Ses 1/Program.cs
+++ W5D2_Training Ses 1/Program.cs
@@ -12,28 +12,36 @@ namespace W5D2_Training_Ses_1
     {
         static void Main(string[] args)
         {
+            
+            
             ///Thread thread_name = n
-            Console.WriteLine("Calling sleep task method");
+            Console.WriteLine("Calling background thread task method");
             Thread thread_name = new Thread(new ThreadStart(sleepTask));  //line 16 and 17 makes the thread asynchronous
+            thread_name.IsBackground = true;
+            Console.WriteLine("thread_name.IsBackground" + thread_name.IsBackground);
             thread_name.Start();
-            thread_name.Join(); // line 19 makes the thread synchronous
+
+            Console.WriteLine("calling Foreground thread taskmethod");
+            Thread Foreground_Thread = new Thread(new ThreadStart(dosomething));
+            Console.WriteLine("thread_name.IsBackground for thread_name_1" + Foreground_Thread.IsBackground);
+            Foreground_Thread.Start();
 
 
-            //sleepTask();
-            Console.WriteLine("calling do something method");
-            dosomething();
+
+
             Console.WriteLine("done with main program");
             Console.ReadLine();
+     
               
         }
 
         public static void sleepTask()           //when creating thread --> create the method first
         {
-            Console.WriteLine("in sleep task");
+            Console.WriteLine("in background task, taking a nap");
             Thread.Sleep(2000);
             Console.WriteLine("do some work");
             Thread.Sleep(5000);
-            Console.WriteLine("i am done working");
+            Console.WriteLine("i am done with background task");
         }
 
         public static void dosomething()
