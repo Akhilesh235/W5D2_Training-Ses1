9a91192cd3b4eb1b85f6f7533013c9d37439a671
Akhilesh235 <90238925+Akhilesh235@users.noreply.github.com>
2021-10-05 10:37:33 +0800

    Add project files.

---
+++ W5D2_Training Ses 1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace W5D2_Training_Ses_1
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Thread thread_name = n
+            Console.WriteLine("Calling sleep task method");
+            sleepTask();
+            Console.WriteLine("calling do something method");
+            dosomething();
+            Console.WriteLine("done with main program");
+            Console.ReadLine();
+              
+        }
+
+        public static void sleepTask()           //when creating thread --> create the method first
+        {
+            Console.WriteLine("in sleep task");
+            Thread.Sleep(2000);
+            Console.WriteLine("do some work");
+            Thread.Sleep(5000);
+            Console.WriteLine("i am done working");
+        }
+
+        public static void dosomething()
+        {
+            Console.WriteLine("perform task quickly");
+        }
+    }
+
+
+}
